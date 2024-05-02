7afe68db2dc4134efe42a3423d25408575b7a478
pipe-justpipe <aremopipeloluwa@gmail.com>
2024-05-02 08:36:39 +0100

    progress update

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,38 +1,56 @@
-import React, { useState } from 'react';
-import TranscendLogo from '../assets/svgs/transcendlogo.svg'; 
+import React, { useState } from "react";
+import TranscendLogo from "../assets/svgs/transcendlogo.svg";
+// import { Link } from 'react-router-dom';
+import { HashLink as Link } from "react-router-hash-link";
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className='flex justify-between mt-12 lg:w-4/5 md:w-[90%] w-[90%] mx-auto'>
-      <img src={TranscendLogo} alt='logo' className='lg:w-[224px] lg:h-[56px] md:w-[180px] md:h-[38px] w-[150px] h-[28px]' />
-      <div className='flex items-center gap-6'>
-        <ul className={`hidden md:flex text-[#1C0F30] text-lg leading-[30px] font-medium gap-6`}>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#pricing">Pricing</a></li>
-          <li><a href="#contact">Contact</a></li>
+    <nav className="flex justify-between mt-12 lg:w-4/5 md:w-[90%] w-[90%] mx-auto">
+      <img
+        src={TranscendLogo}
+        alt="logo"
+        className="lg:w-[224px] lg:h-[56px] md:w-[180px] md:h-[38px] w-[150px] h-[28px]"
+      />
+      <div className="flex items-center gap-6">
+        <ul
+          className={`hidden md:flex text-[#1C0F30] text-lg leading-[30px] font-medium gap-6`}
+        >
+          <Link to={"#home"}>Home</Link>
+          <Link to={"#about"}>About</Link>
+          <Link to={"#pricing"}>Pricing</Link>
+          <Link to={"#contact"}>Contact</Link>
         </ul>
         {!isOpen && (
-          <button className='hidden md:flex py-3 px-6 text-sm text-white bg-gradient-to-br from-purple-600 to-indigo-600 rounded-md'>Get the App</button>
+      <Link to={"#contact"} className="py-3 px-6 text-sm text-white bg-gradient-to-br from-purple-600 to-indigo-600 rounded-md">
+      Get the App
+      </Link>
         )}
-        <div className='md:hidden flex flex-col items-center gap-6'>
-          {!isOpen ? ( 
-            <div className='text-2xl flex flex-end' onClick={() => setIsOpen(!isOpen)}>&#9776;</div>
-            
+        <div className="md:hidden flex flex-col items-center gap-6">
+          {!isOpen ? (
+            <div
+              className="text-2xl flex flex-end"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              &#9776;
+            </div>
           ) : (
-            <div className='text-2xl' onClick={() => setIsOpen(!isOpen)}>&#10006;</div>
+            <div className="text-2xl" onClick={() => setIsOpen(!isOpen)}>
+              &#10006;
+            </div>
           )}
           {isOpen && (
-            <div className='flex flex-col border border-[#1C0F30] px-[20px] py-[16px] rounded-[12px]'>
-              <ul className='text-[#1C0F30] text-lg leading-[30px] font-medium'>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#pricing">Pricing</a></li>
-                <li><a href="#contact">Contact</a></li>
+            <div className="flex flex-col border border-[#1C0F30] px-[20px] py-[16px] rounded-[12px]">
+              <ul className="text-[#1C0F30] text-lg leading-[30px] font-medium">
+                <Link to={"#home"}>Home</Link>
+                <Link to={"#about"}>About</Link>
+                <Link to={"#pricing"}>Pricing</Link>
+                <Link to={"#contact"}>Contact</Link>
               </ul>
-              <button className='py-3 px-6 text-sm text-white bg-gradient-to-br from-purple-600 to-indigo-600 rounded-md'>Get the App</button>
+              <Link to={"#contact"} className="py-3 px-6 text-sm text-white bg-gradient-to-br from-purple-600 to-indigo-600 rounded-md">
+                Get the App
+                </Link>
             </div>
           )}
         </div>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,33 +1,71 @@
 import React from "react";
-import Heading from "../Components/Heading";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
+
   return (
     <section id="contact" className="lg:mt-[150px] md:mt-[100px] mt-[70px]">
-          {/* <Heading speed={50}>
-           Contact Us
-  
-      </Heading> */}
-      <h1 className="text-[#1C0F30] lg:text-[40px] md:text-[30px] text-[25px]  leading-[50px] text-center font-medium " id="demo">Contact Us</h1>
-
-      <p className="w-[1164px] max-[1180px]:w-4/5 text-center mx-auto lg:text-[20px] md:text-[16px] text-[12px] leading-[30px] text-[#31135E] font-normal mt-[50px] mb-[40px] md:mb-[70px] lg:mb-[100px]">
+      <h1
+        className="text-[#1C0F30] lg:text-[40px] md:text-[30px] text-[25px] leading-[50px] text-center font-medium"
+        id="demo"
+      >
+        Contact Us
+      </h1>
+      <p className="w-full max-w-[1180px] text-center mx-auto lg:text-[20px] md:text-[16px] text-[12px] leading-[30px] text-[#31135E] font-normal mt-[50px] mb-[40px] md:mb-[70px] lg:mb-[100px]">
         Have a question, comment, or feedback for us? We’d love to hear from
         you! Our team is dedicated to providing the best possible experience,
         and we're always eager to assist you.
       </p>
       <div className="w-4/5 mx-auto rounded-[50px] border border-solid py-[60px] border-[#BE95FF]">
-        <form className="mx-auto flex flex-col gap-[40px]">
+        <form
+          className="mx-auto flex flex-col gap-[40px]"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="flex flex-wrap gap-[20px] justify-between w-4/5 mx-auto">
             <input
-              className="border border-solid border-black border-opacity-25 lg:w-[480.18px] md:w-[380px] w-[200px] mx-auto rounded-[12px] px-[20px] py-[16px]"
-              placeholder="what is your name? *"
+              className="border outline-none border-solid border-black border-opacity-25 lg:w-[480.18px] md:w-[380px] w-full mx-auto rounded-[12px] px-[20px] py-[16px]"
+              placeholder="What is your name? *"
+              {...register("name", { required: "Name is required" })}
             />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name.message}</p>
+            )}
+
             <input
-              className="border border-solid border-black border-opacity-25 lg:w-[480.18px] md:w-[380px] w-[200px] mx-auto rounded-[12px] px-[20px] py-[16px]"
+              className="border outline-none border-solid border-black border-opacity-25 lg:w-[480.18px] md:w-[380px] w-full mx-auto rounded-[12px] px-[20px] py-[16px]"
               placeholder="Email Address"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email format",
+                },
+              })}
             />
-          </div>{" "}
-          <textarea className="border-2 border-solid border-[#642AC3] w-4/5 mx-auto h-[212px] flex justify-center rounded-[12px] " />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+          </div>
+
+          <textarea
+            className="border-2 outline-none py-[16px] px-[20px] border-solid border-[#642AC3] w-4/5 mx-auto h-[212px] flex justify-center rounded-[12px]"
+            {...register("message", { required: "Message is required" })}
+          />
+          {errors.message && (
+            <p className="text-red-500 text-sm">{errors.message.message}</p>
+          )}
+
+<div className="flex justify-end w-4/5 mx-auto"><button className="py-[16px] px-[20px] w-[137px] text-sm text-white bg-gradient-to-br from-purple-600 to-indigo-600 rounded-md">Submit</button>
+        </div>
         </form>
       </div>
     </section>
